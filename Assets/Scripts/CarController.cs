60c922d2bd6c60ec7104532488e7a930ad57e725
Yamooza <einovalta@gmail.com>
2024-09-23 12:46:44 +0300

    Car script somewhat working

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,9 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    [Header("Car Settings")]
+    public float accelerationFactor = 30.0f;
+    public float turnFactor = 3.5f;
+
+    //Local variables
+    float accelerationInput = 0;
+    float steeringInput = 0;
+
+    float rotationAngle = 0;
+
+
+    //components
+    Rigidbody2D carRigibody2D;
+
+    //Awake is called when the script instance is being loaded
+    private void Awake()
+    {
+        carRigibody2D = GetComponent<Rigidbody2D>();
+    }
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +36,34 @@ public class CarController : MonoBehaviour
     {
         
     }
+
+    private void FixedUpdate()
+    {
+        ApplyEngineForce();
+
+        ApplySteering();
+    }
+    void ApplyEngineForce()
+    {
+        //Create force for the engine
+        Vector2 engineForceVector = transform.up * accelerationInput * accelerationFactor;
+
+        //apply force to push the car
+        carRigibody2D.AddForce(engineForceVector, ForceMode2D.Force);
+    }
+    void ApplySteering()
+    {
+        //update the rotation angle baseed on input
+        rotationAngle -= steeringInput * turnFactor;
+
+        //apply steering by rotating the car object
+        carRigibody2D.MoveRotation(rotationAngle);
+    }
+    public void SetInputVector(Vector2 inputVector)
+    {
+        steeringInput = inputVector.x;
+        accelerationInput = inputVector.y;
+    }
+
+
 }
---
+++ Assets/Scripts/CarInputHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarInputHandler : MonoBehaviour
+{
+    //components
+    CarController carController;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        carController = GetComponent<CarController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 inputVector = Vector2.zero;
+
+        inputVector.x = Input.GetAxis("Horizontal");
+        inputVector.y = Input.GetAxis("Vertical");
+
+        carController.SetInputVector(inputVector);
+    }
+}
